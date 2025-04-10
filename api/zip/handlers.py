d946f3cfe953b69c563bfa19405cf2bbd1a42ad3
josephazar <joseph_azar@outlook.com>
2025-04-10 08:54:44 +0200

    remove api folder

--- api/zip/handlers.py
+++
@@ -1,8 +0,0 @@
-from fastapi.encoders import jsonable_encoder
-from fastapi.exceptions import RequestValidationError
-from starlette import status
-from starlette.responses import JSONResponse, FileResponse
-
-from app import app
-from fastapi import Request
-
